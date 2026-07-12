f26973453e8e05742b4986ca78f47ed3b192d405
Bhupendra-17 <bhupendrad1724@gmail.com>
2026-07-12 16:20:11 +0530

    updated url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,11 +2,12 @@ import axios from "axios";
 
 // Create an Axios instance
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
+
 
 // Request Interceptor: Attach token to every request if it exists
 api.interceptors.request.use(
